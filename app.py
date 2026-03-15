607472ec8c757f545d481556ba9206d0822cb195
Disham Lamjana <dishamsmt@gmail.com>
2026-03-16 00:02:23 +0700

    Commit 2: Add jobs listing page

--- app.py
+++ app.py
@@ -8,3 +8,23 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# หน้าที่ 2: รายการงาน
+@app.route('/jobs')
+def jobs():
+    return render_template('jobs.html')
+
+# หน้าที่ 3: เกี่ยวกับเรา
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
